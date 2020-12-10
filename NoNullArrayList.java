7a118c0f3a8e27265bb3338972ed0741e2e83616
Igbathscaller <Igbathscaller@gmail.com>
2020-12-10 15:18:51 -0500

    stuff

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -7,4 +7,24 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     public NoNullArrayList(int intialCapacity){
         super(intialCapacity);
     }
+
+    public boolean add(T element){
+        if (element == null){
+            throw new IllegalArgumentException("NULL");
+        }
+        else{
+            return this.add(element);
+        }
+    }
+
+    public void add(int index, T element){
+        if (element == null){
+            throw new IllegalArgumentException("NULL");
+        }
+        else{
+            this.add(index,element);
+        }
+    }
+
+    
 }
