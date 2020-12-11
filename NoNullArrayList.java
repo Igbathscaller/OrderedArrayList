8c4315d98edf0550bdb11c30f6dd6017e07b5399
Igbathscaller <Igbathscaller@gmail.com>
2020-12-10 20:24:18 -0500

    orderedArrayList

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
- 
+
 public class NoNullArrayList<T> extends ArrayList<T>{
     public NoNullArrayList(){
         super();
@@ -26,5 +26,13 @@ public class NoNullArrayList<T> extends ArrayList<T>{
         }
     }
 
-    
+    public T set(int index, T element){
+        if (element == null){
+            throw new IllegalArgumentException("NULL");
+        }
+        else{
+            return this.set(index,element);
+        }
+    }
+
 }
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,1 +1,21 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+    
+    public OrderedArrayList(){
+        super();
+    }
 
+    public OrderedArrayList(int Cap){
+        super(Cap);
+    }
+
+    public boolean add(T element){
+        if (element == null){
+            throw new IllegalArgumentException("NULL");
+        }
+        else{
+            return this.add(element);
+        }
+    }
+
+    
+}
