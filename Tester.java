83d00070b7a4010f75b63d593eb6397b7a40265b
Igbathscaller <Igbathscaller@gmail.com>
2020-12-10 20:58:17 -0500

    tester improvements

--- Tester.java
+++ Tester.java
@@ -1,9 +1,14 @@
 public class Tester {
     public static void main(String[]args){
         OrderedArrayList<Integer> k = new OrderedArrayList<Integer>();
-        for(int i = 0; i<args.length; i++){
+        for(int i = 0; i<args.length-2; i++){
             k.add(Integer.parseInt(args[i]));
         }
         System.out.println(k);
+        
+        if(args.length>2){
+            k.set(Integer.parseInt(args[args.length-2]),Integer.parseInt(args[args.length-1]));
+            System.out.println(k);
+        }
     }
 }
