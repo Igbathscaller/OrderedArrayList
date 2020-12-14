43a418fbaf698321d1b5e96a0756a5d3b934827d
Igbathscaller <Igbathscaller@gmail.com>
2020-12-13 22:48:55 -0500

    overrided add at index

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -14,12 +14,25 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
         }
         int index = 0;
         for(int i = 0; i<super.size() && i == index;i++){
-            if(super.get(i).compareTo(element)<0){
+            if(super.get(i).compareTo(element)<=0){
                 index++;
             }
         }
         super.add(index,element);
         return true;
+    }
+
+    public void add(int useless, T element){
+        if (element == null){
+            throw new IllegalArgumentException("NULL");
+        }
+        int index = 0;
+        for(int i = 0; i<super.size() && i == index;i++){
+            if(super.get(i).compareTo(element)<=0){
+                index++;
+            }
+        }
+        super.add(index,element);
     }
 
     public T set(int index,T element){
